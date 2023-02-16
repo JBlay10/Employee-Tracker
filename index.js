54d737a05407dd23dbc634be63280bd5b444a4fd
JBlay10 <joseblay10@gmail.com>
2023-02-16 16:39:44 -0500

    Worked on connection and setup index prompt options, added .env file

---
+++ index.js
@@ -0,0 +1,76 @@
+const inquirer = require('inquirer');
+const db = require('./config/connection')
+require('console.table');
+
+
+db.connect((err) => {
+    if (err) throw err;
+    start();
+})
+
+// This will be the main menu options for adding and viewing the db
+const promptOptions = { 
+    viewDepart: "View Departments",
+    viewRoles: "View Roles",
+    viewEmployees: "View Employees",
+    addDepart: "Add Department",
+    addRole: "Add Role",
+    addEmployee: "Add Employee",
+    updateEmployee: "Update Employee",
+    exit: "Exit"
+};
+
+// Inquirer questions and selections
+function menu() {
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "menu",
+            message: "Let's get started. What would you like to do?",
+            choices: [
+                promptOptions.viewDepart,
+                promptOptions.viewRoles,
+                promptOptions.viewEmployees,
+                promptOptions.addDepart,
+                promptOptions.addRole,
+                promptOptions.addEmployee,
+                promptOptions.updateEmployee,
+                promptOptions.exit
+            ]
+        }
+    ])
+}
+
+// functions that will be called after a prompt is selected
+function viewDepart() {
+
+}
+
+function viewRoles() {
+    
+}
+
+function viewEmployees() {
+    
+}
+
+function addDepart() {
+    
+}
+
+function addRole() {
+    
+}
+
+function addEmployee() {
+    
+}
+
+function updateEmployee() {
+    
+}
+
+function exit() {
+
+}
