8d284f2270cd38156c5fb1dd7ed5b305ab306d7b
JBlay10 <joseblay10@gmail.com>
2023-02-17 18:40:40 -0500

    finished index functions, agot new error where tables are not bieng found

--- index.js
+++ index.js
@@ -40,40 +40,83 @@ function menu() {
             ]
         }
     ])
+    .then(answer => {
+        switch (answer.menu) {
+            case promptOptions.viewDepart:
+                viewDepart();
+                break;
+
+            case promptOptions.viewRoles:
+                viewRoles();
+                break;
+
+            case promptOptions.viewEmployees:
+                viewEmployees();
+                break;
+
+            case promptOptions.addDepart:
+                addDepart();
+                break;
+
+            case promptOptions.addRole:
+                addRole();
+                break;
+
+            case promptOptions.addEmployee:
+                addEmployee();
+                break;
+
+            case promptOptions.updateEmployee:
+                updateEmployee();
+                break;
+
+            case promptOptions.exit:
+                exit();
+                break;
+        }
+    });
 }
 
 // functions that will be called after a prompt is selected
 function viewDepart() {
+    db.promise().query('SELECT * FROM department').then(departmentInfo => {
+        console.table(departmentInfo[0])
+        menu();
+    })
+}
+
+function viewRoles() {
+    db.promise().query("SELECT * FROM employee_roles").then(rolesInfo => {
+        console.table(rolesInfo[0])
+        menu();
+    })
+}
+
+function viewEmployees() {
     db.promise().query('SELECT * FROM employee').then(employeeInfo => {
         console.table(employeeInfo[0])
         menu();
     })
 }
 
-function viewRoles() {
-    
-}
-
-function viewEmployees() {
-    
-}
-
 function addDepart() {
-    
+    menu();
 }
 
 function addRole() {
-    
+    menu();
 }
 
 function addEmployee() {
-    
+    menu();
 }
 
 function updateEmployee() {
-    
+    menu();
 }
 
 function exit() {
-
+    menu();
 }
+
+menu();
